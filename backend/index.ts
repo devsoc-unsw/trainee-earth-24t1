0fa3d8c607e2fc46a9e3060280dde01ca688c9c1
skyuanow <skyuanow@gmail.com>
2024-04-07 11:41:27 +1000

    fixed api key issue

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const  express  = require('express');
 const { OpenAI } = require('openai');
 
 const app = express();
 const port = 3000;
-const openai = new OpenAI({ apiKey: "addkeyhere",});
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY,});
 
 app.get('/', (req, res) => {
   res.send('haiii guys');
@@ -13,17 +16,24 @@ app.listen(port, () => {
   console.log(`Earth app listening on port ${port}`);
 });
 
-app.get('/gen', (req, res) => {
-  res.send(generateImage());
+app.get('/gen', async (req, res) => {
+  try {
+    const data = await generateImage();
+    const imageURL = data.data[0].url;
+    res.send(`<html><body><img src="${imageURL}" /></body></html>`);
+  } catch (err) {
+    console.log('oopsie error');
+    res.status(500).send('oopsiees')
+  }
 });
 
 async function generateImage() {
   const data = await openai.images.generate({
     model: "dall-e-3",
-    prompt: "isometric perspective of a square base house. Cottage core theme. Keep all borders of house within image. Plain white background.",
+    prompt: "isometric perspective of a square base house. Cottage core theme. Keep all borders of house within image. Plain white transparent background.",
     n: 1,
     size: "1024x1024",
   });
 
-  console.log(data);
+  return data;
 }
