b8130d0083dfb56a8b877b8e1df2c3b6e1e0065f
Gordon Huang <dqna64@gmail.com>
2024-05-09 02:06:37 +1000

    improve gameloop efficiency by reducing setTimeout calls

--- backend/src/gameloopFramework.ts
+++ backend/src/gameloopFramework.ts
@@ -2,11 +2,14 @@
  * Adapted from https://github.com/timetocode/node-game-loop
  */
 
+import assert from "assert";
+
 export type UpdateFn = (delta: number) => void;
+type GameLoopFn = () => void;
 
 /**
  * Gameloop framework. Inject your custom game update function. This framework
- * will run your update function at a specified framerate (tickLengthMs)
+ * will run your update function at a specified framerate (frameLengthMs)
  *
  * Usage:
  *      const myUpdateFn = () => {console.log("Update");};
@@ -21,7 +24,24 @@ export class GameLoop {
    * Length of a tick in milliseconds. The denominator is your desired framerate.
    * e.g. 1000 / 20 = 20 fps,  1000 / 60 = 60 fps
    */
-  readonly tickLengthMs = 1000 / 20;
+  readonly frameLengthMs = 1000 / 4;
+
+  /**
+   * Period of time right before the next frame is intended to begin,
+   * during which we make the gameloop use setImmediate instead of setTimeout
+   * to schedule the next gameloop tick. According to article below, nodejs
+   * setTimeout schedules the next tick within a 16-32ms error margin.
+   * https://timetocode.tumblr.com/post/71512510386/
+   */
+  readonly accurateTickPeriodMs = 16;
+
+  /**
+   * Minimum delay in milliseconds for setTimeout. Must be smaller than
+   * accurateTickPeriodMs so that the gameloop will always have a chance
+   * to use setImmediate to schedule the next tick, before a setTimeout
+   * delay overshoots the intended time of next tick.
+   */
+  readonly minSetTimeoutDelayMs = 10;
 
   /* gameLoop related variables */
   // timestamp of each loop
@@ -31,40 +51,68 @@ export class GameLoop {
 
   private readonly update: UpdateFn;
 
+  private gameLoop: GameLoopFn;
+
   constructor(update: UpdateFn) {
     this.update = update;
-  }
 
-  private gameLoop() {
-    var now = Date.now();
-
-    this.actualTicks++;
-    if (this.previousTick + this.tickLengthMs <= now) {
-      var delta = (now - this.previousTick) / 1000;
-      this.previousTick = now;
-
-      this.update(delta);
-
-      console.log(
-        "delta",
-        delta,
-        "(target: " + this.tickLengthMs + " ms)",
-        "node ticks",
-        this.actualTicks
-      );
-      this.actualTicks = 0;
-    }
-
-    if (Date.now() - this.previousTick < this.tickLengthMs - 16) {
-      setTimeout(this.gameLoop);
-    } else {
-      setImmediate(this.gameLoop);
-    }
+    assert(
+      this.accurateTickPeriodMs > this.minSetTimeoutDelayMs,
+      "accurateTickPeriodMs must be greater than minSetTimeoutDelayMs"
+    );
   }
 
   startGameLoop() {
     this.previousTick = Date.now();
     this.actualTicks = 0;
+    let setTimeoutCount = 0;
+    let setImmediateCount = 0;
+
+    if (!this.gameLoop) {
+      const gameLoop = () => {
+        let now = Date.now();
+        let progress = now - this.previousTick;
+
+        this.actualTicks++;
+        if (progress >= this.frameLengthMs) {
+          var delta = now - this.previousTick;
+          this.previousTick = now;
+
+          this.update(delta);
+
+          console.log(
+            `delta ${delta}ms;`,
+            `target: ${this.frameLengthMs} ms;`,
+            `node ticks: ${this.actualTicks};`,
+            `setTimeoutCount: ${setTimeoutCount};`,
+            `setImmediateCount: ${setImmediateCount};`
+          );
+          this.actualTicks = 0;
+          setTimeoutCount = 0;
+          setImmediateCount = 0;
+        }
+
+        now = Date.now();
+        progress = now - this.previousTick;
+        if (progress < this.frameLengthMs - this.accurateTickPeriodMs) {
+          setTimeoutCount++;
+          setTimeout(
+            gameLoop,
+            Math.max(
+              (this.frameLengthMs - this.accurateTickPeriodMs - progress) / 2,
+              this.minSetTimeoutDelayMs
+            )
+            // setTimout delay must be lower-bounded at some value smaller than 16 in order
+            // to avoid the inacurrate setTimeout delay from "overshooting"
+            // beyond this.previousTick + this.frameLengthMs.
+          );
+        } else {
+          setImmediateCount++;
+          setImmediate(gameLoop);
+        }
+      };
+      this.gameLoop = gameLoop;
+    }
 
     this.gameLoop();
   }
