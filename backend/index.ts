ea7c41c4347e2ccbb35282d569f0d9f7d2c71a22
skyuanow <skyuanow@gmail.com>
2024-05-09 13:52:41 +1000

    implemented remove bg thing

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv');
 const axios = require('axios');
+const FormData = require('form-data');
 const fs = require('fs');
 dotenv.config();
 
@@ -96,4 +97,30 @@ async function generateImage(prompt) {
   });
 
   return data;
+}
+
+// Remove bg
+function removeImageBG(url) {
+  const formData = new FormData();
+  formData.append('size', 'auto');
+  formData.append('image_url', url);
+  
+  axios({
+    method: 'post',
+    url: 'https://api.remove.bg/v1.0/removebg',
+    data: formData,
+    responseType: 'arraybuffer',
+    headers: {
+      ...formData.getHeaders(),
+      'X-Api-Key': process.env.REMOVEBG_KEY,
+    },
+    encoding: null
+  })
+  .then((response) => {
+    if(response.status != 200) return console.error('Error:', response.status, response.statusText);
+    fs.writeFileSync("anothaone.png", response.data);
+  })
+  .catch((error) => {
+      return console.error('Request failed:', error);
+  });
 }
