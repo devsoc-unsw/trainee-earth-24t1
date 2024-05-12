3d3a7dbe15f3c15b7539f37ea6a15c7c62f52d4c
Gordon Huang <dqna64@gmail.com>
2024-05-13 02:18:31 +1000

    merge main frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,38 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./Navbar";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./components/ui/card";
 
-function App() {
+export default function App() {
   return (
-    <>
-      <h1>hello team earth!!</h1>
-    </>
+    <body>
+      <header className="header">
+        <Navbar />
+      </header>
+      <div className="content mx-auto w-full max-w-screen-xl">
+        <Button variant="outline">Button</Button>
+        <br />
+        <br />
+        <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>{" "}
+      </div>
+    </body>
   );
 }
-
-export default App;
