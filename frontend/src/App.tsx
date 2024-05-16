2297b14c7ee66dfab5a9be97a5c3d055e7be6897
Dylan Zhang <z5478419@ad.unsw.edu.au>
2024-05-16 21:15:53 +1000

    sync widget

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,17 @@
+import { Calendar } from "lucide-react";
 import "./App.css";
 import Navbar from "./Navbar";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "./components/ui/card";
+import Timer from "./components/ui/timer";
 
 export default function App() {
   return (
-    <body>
+    <div>
       <header className="header">
         <Navbar />
       </header>
-      <div className="content mx-auto w-full max-w-screen-xl">
-        <Button variant="outline">Button</Button>
-        <br />
-        <br />
-        <Card>
-          <CardHeader>
-            <CardTitle>Card Title</CardTitle>
-            <CardDescription>Card Description</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>{" "}
+      <div className="content mx-auto w-full max-w-screen-xl justify-center ">
+        <Timer />
       </div>
-    </body>
+    </div>
   );
 }
---
+++ frontend/src/components/ui/timer.tsx
@@ -0,0 +1,15 @@
+export default function Timer() {
+  return (
+    <section>
+      <div className="bg-white rounded-2xl h-60 w-60 flex justify-center content-center">
+        <div className="flex justify-center content-center items-center">
+          <div className="bg-green-400 h-48 w-48 rounded-full flex justify-center items-center">
+            <div className="bg-white h-36 w-36 rounded-full flex justify-center items-center">
+              <p><b>Timer</b></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
