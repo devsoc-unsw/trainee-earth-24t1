dbe44c2ecb59aacea6d54b027241e9bc85052b65
dylan <robouser51@gmail.com>
2024-05-21 02:20:17 +1000

    displaying widgets

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
-import { Calendar } from "lucide-react";
 import "./App.css";
 import Navbar from "./Navbar";
 import Timer from "./components/ui/timer";
+import { Calendar } from "./components/ui/calendar";
 
 export default function App() {
   return (
@@ -9,8 +9,12 @@ export default function App() {
       <header className="header">
         <Navbar />
       </header>
-      <div className="content mx-auto w-full max-w-screen-xl justify-center ">
-        <Timer />
+      <div className="content flex-col mx-auto w-full max-w-screen-xl justify-center items-center">
+        {'widgets'}
+        <div className="flex justify-around w-100">
+          <Calendar />
+          <Timer />
+        </div>
       </div>
     </div>
   );
