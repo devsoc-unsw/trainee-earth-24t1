ff716c63427f55ca1d759073cc2ec3074f7ec87b
dylan <robouser51@gmail.com>
2024-05-29 05:14:09 +1000

    testing for inventory popup

--- frontend/src/Navbar.tsx
+++ frontend/src/Navbar.tsx
@@ -6,7 +6,18 @@ import {
   SignedOut,
   UserButton,
 } from "@clerk/clerk-react";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@frontend/@/components/ui/dialog"
 import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
 
 const Navbar = () => {
   return (
@@ -20,6 +31,41 @@ const Navbar = () => {
         <Button onClick={() => localStorage.clear()}>
           Reset Local Storage
         </Button>
+
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" className="bg-white">Edit Profile</Button>
+          </DialogTrigger>
+          <div className="w-full h-full">
+            <DialogContent className="sm:max-w-[425px] bg-white relative rounded-md">
+              <DialogHeader>
+                <DialogTitle>Edit profile</DialogTitle>
+                <DialogDescription>
+                  Make changes to your profile here. Click save when you're done.
+                </DialogDescription>
+              </DialogHeader>
+              <div className="grid gap-4 py-4">
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Input
+                    id="name"
+                    defaultValue="Pedro Duarte"
+                    className="col-span-3 bg-white"
+                  />
+                </div>
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Input
+                    id="username"
+                    defaultValue="@peduarte"
+                    className="col-span-3 bg-white"
+                  />
+                </div>
+              </div>
+              <DialogFooter>
+                <Button type="submit">Save changes</Button>
+              </DialogFooter>
+            </DialogContent>
+          </div>
+        </Dialog>
 
         <div className="user-icon">
           <SignedIn>
