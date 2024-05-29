5372a8a2c8990f9556995e484c882d0fc357ac08
dylan <robouser51@gmail.com>
2024-05-30 02:15:07 +1000

    changed navbar

--- frontend/src/Navbar.tsx
+++ frontend/src/Navbar.tsx
@@ -18,8 +18,13 @@ import {
 } from "@frontend/@/components/ui/dialog"
 import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
+import { ReactNode } from "react";
 
-const Navbar = () => {
+interface NavbarProps {
+  children: ReactNode;
+}
+
+const Navbar = ({ children }: NavbarProps) => {
   return (
     <nav className="navbar py-1 px-4 w-full max-w-screen-2xl">
       <a className="logo">
@@ -32,40 +37,7 @@ const Navbar = () => {
           Reset Local Storage
         </Button>
 
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button variant="outline" className="bg-white">Edit Profile</Button>
-          </DialogTrigger>
-          <div className="w-full h-full">
-            <DialogContent className="sm:max-w-[425px] bg-white relative rounded-md">
-              <DialogHeader>
-                <DialogTitle>Edit profile</DialogTitle>
-                <DialogDescription>
-                  Make changes to your profile here. Click save when you're done.
-                </DialogDescription>
-              </DialogHeader>
-              <div className="grid gap-4 py-4">
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Input
-                    id="name"
-                    defaultValue="Pedro Duarte"
-                    className="col-span-3 bg-white"
-                  />
-                </div>
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Input
-                    id="username"
-                    defaultValue="@peduarte"
-                    className="col-span-3 bg-white"
-                  />
-                </div>
-              </div>
-              <DialogFooter>
-                <Button type="submit">Save changes</Button>
-              </DialogFooter>
-            </DialogContent>
-          </div>
-        </Dialog>
+        {children}
 
         <div className="user-icon">
           <SignedIn>
