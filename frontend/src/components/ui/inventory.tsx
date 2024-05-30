e37c0e6d2e52d33f74983d7370ac5399fbd55ba4
dylan <robouser51@gmail.com>
2024-05-30 22:08:31 +1000

    implemented inventory ui for resources

---
+++ frontend/src/components/ui/inventory.tsx
@@ -0,0 +1,38 @@
+import { AnimatedGradientBorder } from './animatedGradientBorder'; // Adjust the import path as necessary
+
+// Sample inventory items
+const inventoryItems = [
+  { id: 1, name: 'Iron', description: "Earth's silent strength, forged anew.", imgSrc: 'https://flatearth.b-cdn.net/production-2024-05-28T22:50:47.100Z/edges-cropped.png' },
+  { id: 2, name: 'Beer', description: 'Yummy yummy', imgSrc: 'https://flatearth.b-cdn.net/production-2024-05-28T22:57:44.145Z/edges-cropped.png' },
+];
+
+export default function Inventory() {
+  const numberOfRows = 3;
+  const numberOfColumns = 7;
+
+  return (
+    <div className="flex flex-col justify-around space-y-4">
+      {Array.from({ length: numberOfRows }).map((_, rowIndex) => (
+        <div key={rowIndex} className="flex justify-around">
+          {Array.from({ length: numberOfColumns }).map((_, colIndex) => {
+            const itemIndex = rowIndex * numberOfColumns + colIndex;
+            const item = inventoryItems[itemIndex];
+            return (
+              <AnimatedGradientBorder key={itemIndex}>
+                <div
+                  className="bg-zinc-900 rounded-md w-20 h-20 flex items-center justify-center text-white text-center p-2"
+                >
+                  {item && (
+                    <div>
+                      <img src={`${item.imgSrc}`} />
+                    </div>
+                  )}
+                </div>
+              </AnimatedGradientBorder>
+            );
+          })}
+        </div>
+      ))}
+    </div>
+  );
+}
