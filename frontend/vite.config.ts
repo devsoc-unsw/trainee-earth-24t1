fcfc26ec52820d45f630474044dd7f86e7fe2bf0
lachlanshoesmith <12870244+lachlanshoesmith@users.noreply.github.com>
2025-11-26 23:28:36 +1100

    chore: migrate to tailwind v4

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,15 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@frontend": path.resolve(__dirname, "./"),
-      "@backend/types": path.resolve(__dirname, "../backend/types"),
-      "@backend/utils": path.resolve(__dirname, "../backend/utils"),
-      "@backend/sample-data": path.resolve(__dirname, "../backend/sample-data"),
+      '@frontend': path.resolve(__dirname, './'),
+      '@backend/types': path.resolve(__dirname, '../backend/types'),
+      '@backend/utils': path.resolve(__dirname, '../backend/utils'),
+      '@backend/sample-data': path.resolve(__dirname, '../backend/sample-data'),
     },
   },
 });
